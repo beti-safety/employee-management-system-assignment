127feb49a1753493686071c3e94547cce50a9ec6
MarkRozovsky <mark.rozovsky@beti.tech>
2025-08-13 17:47:03 +0300

    fix vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     host: "0.0.0.0",
     port: Number(process.env.PORT) || 5173,
     strictPort: true,
+    allowedHosts: "all",
     proxy: {
       "/api": {
         target: "http://localhost:3001",
